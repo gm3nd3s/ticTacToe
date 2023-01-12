e28dc377b79e5380ec7be9f2ffdd7aaaa1407045
gm3nd3s <gmbasquet@hotmail.com>
2023-01-12 14:24:19 +0000

    Added dashboard score styles

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -25,8 +25,30 @@ const Score = styled.div`
 	display: flex;
 	flex-direction: row;
 	gap: 10px;
+	justify-content: space-around;
+	margin: 0 auto;
+`;
+const Plate = styled.span`
+	background-color: black;
+	color: white;
+	width: 22px;
+	height: 22px;
+	align-self: center;
+	text-align: center;
 `;
 
+const ScoreWrapperLeft = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	gap: 6px;
+	width: 140px;
+`;
+const ScoreWrapperRight = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	gap: 6px;
+	width: 140px;
+`;
 export const Dashboard = () => {
 	const score = useSelector((state) => state.score);
 	const dispatch = useDispatch();
@@ -34,8 +56,14 @@ export const Dashboard = () => {
 		<DashboardStyle>
 			<h2>Score</h2>
 			<Score>
-				<p>{`Player 1: ${score.player1} `}</p>
-				<p>{`Player 2: ${score.player2} `}</p>
+				<ScoreWrapperLeft>
+					<p>PLAYER</p>
+					<Plate>{score.player1} </Plate>
+				</ScoreWrapperLeft>
+				<ScoreWrapperRight>
+					<Plate>{score.player2} </Plate>
+					<p>COMPUTER</p>
+				</ScoreWrapperRight>
 			</Score>
 			<Reset
 				onClick={() => {
